3b65d1124e6ff189ce84bb7719bfa2110f8a3920
rishi322 <rishipatel1850@gmail.com>
2026-07-06 13:38:37 +0100

    reverting changes

--- onlineClients.js
+++ onlineClients.js
@@ -1,114 +1,3 @@
-// const WebSocket = require('ws');
-// const port = 7070; // Set the desired port 
-// const wss = new WebSocket.Server({ port });
-// // Shared state of the to-do list 
-
-
-// const games = [];
-
-// ///{"gameid":"",moves:[{move1},{move2}]}
-
-// let totalUser = 0;
-// const mongoose = require('mongoose');
-// const activeGames = require('./Models/active-games');
-// const game = require('./Models/game');
-
-// const dbUser = 'rishi';          // e.g. "rishi"
-// const dbPass = 'Rishi@322002';          // e.g. "Rishi@322002"
-// const dbName = '?appName=Cluster0' 
-// const clusterHost = 'cluster0.m9jczur.mongodb.net';
-
-// // encode credentials in case they have special chars
-// const encodedUser = encodeURIComponent(dbUser || '');
-// const encodedPass = encodeURIComponent(dbPass || '');
-
-// // Example SRV URI with database name and recommended options
-// const MONGO_URI = process.env.MONGODB_URI ||
-//   `mongodb+srv://${encodedUser}:${encodedPass}@${clusterHost}/${dbName}?retryWrites=true&w=majority`;
-//   mongoose.connect(MONGO_URI);
-//   console.log('Connected to MongoDB');
-// wss.on('listening', () => {
-//     console.log(`WebSocket server is listening on port ${port}`);
-// });
-// wss.on('connection', ws => {
-//     totalUser++;
-//     wss.clients.forEach(client=>{
-//         client.send(totalUser)
-//     })
-//     // Send the current to-do list to the newly connected client 
-//     ws.send(totalUser);
-//     console.log(totalUser)
-  
-//     ws.on('message',async (message) => {
-
-//         const msg = JSON.parse(message)
-//         console.log(msg)
-//       if(msg.type == 'message'){
-       
-//           const parsedData = JSON.parse(message);
-//           console.log(parsedData); // Log the entire parsed object
-//           if (parsedData && parsedData.gameId !== undefined) {
-//             const gameId = parsedData.gameId;
-//             console.log('Game ID:', gameId);
-//           } else {
-//             console.error('Invalid or unexpected format in parsed JSON.');
-//           }
-      
-
-//       }else if (msg.type == 'join'){
-//         console.log(msg)
-//         games.forEach(game=>{
-
-//           wss.clients.forEach(client=>{
-              
-//          client.send(JSON.stringify([{type:'start',msg:msg.message,gameId:msg.gameId}]))
-//           })
-
-//         })
-//       }else if(msg.type =='create'){
-//         console.log(msg)
-     
-//         games.push({gameId: msg.message, moves: '',status:'created',player:1});
-//         console.log('game created',games)
-//         const acg = await activeGames.create({gameId: msg.message, moves: '',status:'created',player:1,creater:msg.creater,player2:''})
-
-//         console.log(acg)
-//       }else if(msg.type == 'moves'){
-
-//         console.log('this is messgae')
-//         const result = await game.find({gameId:msg.gameId});
-       
-//           game.create({gameId:msg.gameId,moves:msg.message,fenString:''})
-          
-        
-//         wss.clients.forEach(client=>{
-//           const mov = JSON.stringify(message)
-//           console.log(msg.message)
-          
-//          client.send(JSON.stringify([{type:'move',msg:msg.message,gameId:msg.gameId}]))
-//         })
-//       }
-//       else{
-//         console.log('no type specified')
-//       }
-        
-       
-//     });
-//     ws.on("moves",async(moves)=>{
-//       console.log(moves)
-//       console.log('this is moves')
-//     })
-//     ws.on('close', async() => {
-//         totalUser--;
-//         console.log(totalUser)
-//         // On WebSocket disconnect, save the games array to the database
-//         wss.clients.forEach(client=>{
-//             client.send(totalUser)
-//         })
-//     })
-// });
-
-
 const WebSocket = require('ws');
 const port = 7070; // Set the desired port 
 const wss = new WebSocket.Server({ port });
@@ -143,32 +32,20 @@ wss.on('listening', () => {
 });
 wss.on('connection', ws => {
     totalUser++;
-
-    // Send structured userCount to all clients (previously sent raw number)
     wss.clients.forEach(client=>{
-        try {
-            client.send(JSON.stringify({ type: 'userCount', count: totalUser }));
-        } catch(e){ /* ignore */ }
+        client.send(totalUser)
     })
-    // Send the current user count to the newly connected client 
-    try { ws.send(JSON.stringify({ type: 'userCount', count: totalUser })); } catch(e) {}
-
+    // Send the current to-do list to the newly connected client 
+    ws.send(totalUser);
     console.log(totalUser)
   
     ws.on('message',async (message) => {
 
-        let msg;
-        try {
-          msg = JSON.parse(message);
-        } catch (e) {
-          console.error('Invalid JSON received:', message);
-          return;
-        }
-
+        const msg = JSON.parse(message)
         console.log(msg)
       if(msg.type == 'message'){
        
-          const parsedData = msg; // already parsed above
+          const parsedData = JSON.parse(message);
           console.log(parsedData); // Log the entire parsed object
           if (parsedData && parsedData.gameId !== undefined) {
             const gameId = parsedData.gameId;
@@ -180,13 +57,13 @@ wss.on('connection', ws => {
 
       }else if (msg.type == 'join'){
         console.log(msg)
-        // keep your games array usage but broadcast a single object not array
-        games.forEach(gameItem=>{
+        games.forEach(game=>{
+
           wss.clients.forEach(client=>{
-            try {
-              client.send(JSON.stringify({ type: 'start', msg: msg.message, gameId: msg.gameId }));
-            } catch(e){ /* ignore */ }
+              
+         client.send(JSON.stringify([{type:'start',msg:msg.message,gameId:msg.gameId}]))
           })
+
         })
       }else if(msg.type =='create'){
         console.log(msg)
@@ -204,12 +81,11 @@ wss.on('connection', ws => {
           game.create({gameId:msg.gameId,moves:msg.message,fenString:''})
           
         
-        // Broadcast a single JSON object for move instead of an array
         wss.clients.forEach(client=>{
-          try {
-            // Send single object with clear fields
-            client.send(JSON.stringify({ type: 'move', msg: msg.message, gameId: msg.gameId, from: msg.from, to: msg.to }));
-          } catch(e){ /* ignore */ }
+          const mov = JSON.stringify(message)
+          console.log(msg.message)
+          
+         client.send(JSON.stringify([{type:'move',msg:msg.message,gameId:msg.gameId}]))
         })
       }
       else{
@@ -227,7 +103,9 @@ wss.on('connection', ws => {
         console.log(totalUser)
         // On WebSocket disconnect, save the games array to the database
         wss.clients.forEach(client=>{
-            try { client.send(JSON.stringify({ type: 'userCount', count: totalUser })); } catch(e) {}
+            client.send(totalUser)
         })
     })
 });
+
+
