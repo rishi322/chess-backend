8c72c226e7015f33c69fbecbeba4bf4ef5379d6c
rishi322 <rishipatel1850@gmail.com>
2026-07-06 10:13:36 +0100

    Added cors to cb.js

--- cb.js
+++ cb.js
@@ -45,7 +45,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: 'https://frabjous-biscuit-538177.netlify.app' }));
 
 // default to relative binary in repo
 const defaultStockfish = path.join(__dirname, 'bin', 'stockfish');
